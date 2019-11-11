4ec9fea87111203c881180bd59602e032b873d2c
Crls2957 <2622415210@qq.com>
2019-11-11 12:07:04 +0800

    ReOrderArray

---
+++ java/nowcoder/sword/ReOrderArray.java
@@ -0,0 +1,30 @@
+class ReOrderArray{
+    public void reOrderArray(int [] array){
+        int oddNum=0;
+        for(int i=0;i<array.length;i++){
+            if(array[i]%2==1){
+                oddNum++;
+            }
+        }
+        int [] tempArray=array.clone();
+        int odd=0;
+        int even=oddNum;
+        for(int i=0;i<tempArray.length;i++){
+            if(tempArray[i]%2==1){
+                array[odd]=tempArray[i];
+                odd++;
+            }else{
+                array[even]=tempArray[i];
+                even++;
+            }
+        }
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int [] array={1,3,4,2,5};
+        ReOrderArray reOrderArray=new ReOrderArray();
+        reOrderArray.reOrderArray(array);
+    }
+}
