db6ae5ba9d3f56dd1c5435ed9efda43da32ae8b8
Crls2957 <2622415210@qq.com>
2019-11-11 13:45:53 +0800

    123

---
+++ java/nowcoder/sword/CountNumof1.java
@@ -0,0 +1,14 @@
+class CountNumof1{
+    public int count(int num){
+        int count = 0;
+        for(int i = 0; i < 32; i++){
+            if((num >>> i & 1) == 1)
+                ++count;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        CountNumof1 countNumof1=new CountNumof1();
+        System.out.println(countNumof1.count(6));
+    }
+}
---
+++ java/nowcoder/sword/Power.java
@@ -0,0 +1,21 @@
+class Power{
+    public double getResult(double base,int power) {
+        double result=base;
+        int p=Math.abs(power);
+        if(p==0){
+            return result=1;
+        }else{
+            for(int i=1;i<p;i++){
+                result*=base;
+            }
+        }
+        if(power<0){
+            result=1/result;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Power power=new Power();
+        System.out.println(power.getResult(10, 2));
+    }
+}
