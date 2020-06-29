2356beff616a85ef48fcaaf30912ebc63a96f0cc
Rosen <luosen7@126.com>
2020-06-29 14:35:13 +0800

    Bit Operation

---
+++ c/nowcoder/sword/addByUsingBitOperation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+using namespace std;
+class Solution{
+	public:
+		int addNum(int num1,int num2){
+			while(num2!=0){
+				int temp=num1^num2;
+				num2=(num1&num2)<<1;
+				num1=temp;
+			}
+			return num1; 
+		}
+}; 
+int main(){
+	Solution solution;
+	int result=solution.addNum(5,4);
+	cout<<result<<endl;
+}
