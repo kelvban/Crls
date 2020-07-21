180ae7ed172c9e2d957b40add30b5d92c1867b98
kelvban <2622415210@qq.com>
2020-07-21 16:47:02 +0800

    Sum

---
+++ java/nowcoder/sword/Sum.java
@@ -0,0 +1,15 @@
+/**
+ * sum of 1+2+3...+n
+ */
+public class Sum {
+    public int Sum_Solution(int n) {
+        int result=n;
+        boolean flag=(n>0)&&(result+=Sum_Solution(n-1))>0;
+        return result;
+    }
+    public static void main(String[] args) {
+        Sum sum=new Sum();
+        int result=sum.Sum_Solution(3);
+        System.out.println(result);
+    }
+}
