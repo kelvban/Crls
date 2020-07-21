e31e158cf7b7648a0926f99aba63e5d6f4599529
kelvban <2622415210@qq.com>
2020-07-21 11:03:44 +0800

    find num

--- java/nowcoder/sword/FindNumInArray.java
+++ java/nowcoder/sword/FindNumInArray.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,28 +10,22 @@ import java.util.Map;
  */
 public class FindNumInArray {
     public int MoreThanHalfNum_Solution(int [] array) {
-        int result=0;
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<array.length;i++){
-            if(map.get(array[i])>(array.length/2)){
-                result=array[i];
-                break;
-            }
-            else if(!map.containsKey(array[i])){
-                map.put(array[i], 1);
-            }
-            else{
-                int temp=map.get(array[i]);
-                temp++;
-                map.put(array[i], temp);
-            }
+        Arrays.sort(array);
+        int half = array.length / 2;
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == array[half])
+                count++;
         }
-        return result;
+        if (count > half)
+            return array[half];
+        else
+            return 0;
     }
-    public static void main(String[] args) {
-        FindNumInArray findNumInArray=new FindNumInArray();
-        int [] array={2,3,2,2,2,5,4};
-        int result=findNumInArray.MoreThanHalfNum_Solution(array);
-        System.out.println(result);
-    }
+    // public static void main(String[] args) {
+    //     FindNumInArray findNumInArray=new FindNumInArray();
+    //     int [] array={2,3,2,2,2,5,4};
+    //     int result=findNumInArray.MoreThanHalfNum_Solution(array);
+    //     System.out.println(result);
+    // }
 }
