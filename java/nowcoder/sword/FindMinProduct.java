00ec166d1292b9df32d2ecb03b6cb8e37ca91e01
kelvban <2622415210@qq.com>
2020-08-18 15:16:45 +0800

    minproduct

---
+++ java/nowcoder/sword/FindMinProduct.java
@@ -0,0 +1,25 @@
+/**
+ * Find two numbers whose sum is the target number, and the product is the smallest 
+ */
+
+import java.util.*;
+class FindMinProduct{
+    public ArrayList<Integer> FindNumbersWithSum(int [] array,int target) {
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        int i=0;
+        int j=array.length-1;
+        while(i<j){
+            int sum=array[i]+array[j];
+            if(sum==target){
+                list.add(array[i]);
+                list.add(array[j]);
+                return list;
+            }else if(sum<target){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return list;
+    }
+}
