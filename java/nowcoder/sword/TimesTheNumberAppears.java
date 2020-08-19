ecc646f59e7a0aee108e150fd3223568ad901027
kelvban <2622415210@qq.com>
2020-08-19 10:58:43 +0800

    times

---
+++ java/nowcoder/sword/TimesTheNumberAppears.java
@@ -0,0 +1,27 @@
+/**
+ * Get the number of times the number appears
+ */
+import java.util.*;
+class TimesTheNumberAppears{
+    public int GetNumberOfK(int [] array , int k) {
+        int result=0;
+        Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for(int i=0;i<array.length;i++){
+            if(array[i]==k){
+                if(map.containsKey(array[i])){
+                    int value=map.get(array[i]);
+                    ++value;
+                    map.put(array[i],value);
+                }else{
+                    int value=1;
+                    map.put(array[i],value);
+                }
+            }
+        }
+         if(map.containsKey(k)){
+             result=map.get(k);
+             return result;
+         }
+        return result;
+    }
+}
