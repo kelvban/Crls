feac8df2eabaa14f3592b7f9ae095b42e19b825d
kelvban <2622415210@qq.com>
2020-09-03 14:35:04 +0800

    rotate

---
+++ java/nowcoder/sword/RotateLeft.java
@@ -0,0 +1,16 @@
+/**
+ * sub-string rotate left
+ */
+
+ import java.util.*;
+
+class RotateLeft{
+    public String LeftRotateString(String str,int n) {
+        int len = str.length();
+        if(len==0) return "";
+        n = n%len;
+        str += str;
+        len=len+n;
+        return str.substring(n,len);
+    }
+ }
