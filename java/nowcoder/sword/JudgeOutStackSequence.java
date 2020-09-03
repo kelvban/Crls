22bff528ea0abd2dc18ec927839996ffc39dbd74
kelvban <2622415210@qq.com>
2020-09-03 10:55:18 +0800

    judge

---
+++ java/nowcoder/sword/JudgeOutStackSequence.java
@@ -0,0 +1,24 @@
+/**
+ * judge the out-stack sequence
+ */
+import java.util.*;
+ class JudgeOutStackSequence{
+    public boolean IsPopOrder(int [] pushA,int [] popA) {
+
+        Stack<Integer> stack=new Stack<Integer>();
+
+        if(pushA.length!=popA.length){
+            return false;
+        }
+        int j=0;
+        for(int i=0;i<pushA.length;i++){
+            stack.push(pushA[i]);
+            while(!stack.isEmpty()&&stack.peek()==popA[j]){
+
+                stack.pop();
+                j++;
+            }
+        }
+        return stack.isEmpty();
+    }
+ }
