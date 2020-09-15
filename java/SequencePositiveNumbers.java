070fd68eb4cdab0abc9a551c53eddb8260fafbf5
kelvban <2622415210@qq.com>
2020-09-15 15:08:13 +0800

    sequence

---
+++ java/SequencePositiveNumbers.java
@@ -0,0 +1,24 @@
+/**
+ * find sequence of continuous positive integers with sum s
+ */
+class SequencePositiveNumbers{
+    public ArrayList<ArrayList<Integer> > FindContinuousSequence(int sum) {
+       ArrayList<ArrayList<Integer>> result=new ArrayList<ArrayList<Integer>>();
+       for(int i=1;i<sum;i++){
+           int temp=0;
+           int j=i;
+           while(temp<sum){
+               temp+=j;
+               j++;
+           }
+           if(temp==sum){
+               ArrayList<Integer> sequence=new ArrayList<Integer>();
+               for(int k=i;k<j;k++){
+                   sequence.add(k);
+               }
+               result.add(sequence);
+           }
+       }
+       return result;
+    }
+}
