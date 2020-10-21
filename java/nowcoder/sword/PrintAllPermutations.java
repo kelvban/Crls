8960a48f5320af6885059021329731362e99ee00
kelvban <2622415210@qq.com>
2020-10-21 17:08:20 +0800

    number k

--- java/nowcoder/sword/PrintAllPermutations.java
+++ java/nowcoder/sword/PrintAllPermutations.java
@@ -2,6 +2,15 @@
  * Print all permutations of characters in the string in dictionary order
  */
 
+ /**
+  * 可以将字符串分为两个部分，
+  第一个字符和剩下其他的字符，
+  先确定第一个字符的所有可能结果，
+  即先与自身交换再剩下的其他字符逐一交换，
+  每交换完一次就相当于确定了一个字符，
+  接着需要确定下一个字符即第二个字符，
+  让它再与自身交换然后与它之后的字符逐一交换…
+  */
 package nowcoder.sword;
 import java.util.ArrayList;
 import java.util.TreeSet;
