f6f30cca83a17d9fdacdc4eb5364b62992e5f551
kelvban <2622415210@qq.com>
2020-10-22 11:04:01 +0800

    minimum number string

--- java/nowcoder/sword/MinimumStringOfNumber.java
+++ java/nowcoder/sword/MinimumStringOfNumber.java
@@ -3,9 +3,39 @@
  */
 package nowcoder.sword;
 
+import java.util.*;
+
 public class MinimumStringOfNumber {
     public String PrintMinNumber(int [] numbers) {
-        String result=null;
-        return result;
+        if(numbers == null){
+            return null;
+        }
+        ArrayList<String> result = new ArrayList<>();
+        recur(numbers,"",result);
+        Collections.sort(result);
+        return result.get(0);
+    }
+ 
+    public void recur(int[] numbers, String cur, ArrayList<String>result){
+        if(numbers.length == 0){
+            for(int i = 0; i < result.size();i++){
+                if(result.get(i) == cur){
+                    return;
+                }
+            }
+            result.add(cur);
+        }
+ 
+        for(int i = 0; i < numbers.length; i++){
+            int[] temp = new int[numbers.length-1];
+            int count = 0;
+            for(int j = 0; j < numbers.length;j++){
+                if(j != i){
+                    temp[count] = numbers[j];
+                    count++;
+                }
+            }
+            recur(temp,cur+Integer.toString(numbers[i]),result);
+        }
     }
 }
