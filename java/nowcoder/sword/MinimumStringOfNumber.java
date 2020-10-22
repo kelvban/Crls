f48f8df3ff0cef1f2db31ef30e098438d468b1e9
kelvban <2622415210@qq.com>
2020-10-22 10:30:08 +0800

    blank

---
+++ java/nowcoder/sword/MinimumStringOfNumber.java
@@ -0,0 +1,11 @@
+/**
+ * get the minimum number combination in array 
+ */
+package nowcoder.sword;
+
+public class MinimumStringOfNumber {
+    public String PrintMinNumber(int [] numbers) {
+        String result=null;
+        return result;
+    }
+}
