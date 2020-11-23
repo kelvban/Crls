0b1047cf490f59a6c5ee02a028c9b8ce886c6186
kelvban <2622415210@qq.com>
2020-11-23 10:03:56 +0800

    serialize

---
+++ java/nowcoder/sword/SerializeBinaryTree.java
@@ -0,0 +1,26 @@
+/**
+ * serialize and deserialize binary tree
+ */
+
+ package nowcoder.sword;
+
+ class BinaryTreeNode{
+     int val;
+     BinaryTreeNode left=null;
+     BinaryTreeNode right=null;
+
+     public BinaryTreeNode(int val){
+         this.val=val;
+     }
+ }
+
+ public class SerializeBinaryTree{
+
+    String Serialize(TreeNode root) {
+        
+    }
+      TreeNode Deserialize(String str) {
+         
+    }
+
+ }
