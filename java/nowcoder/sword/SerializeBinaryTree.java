4d3fd64b2bf229839c1ffe308a256fd042fb615f
kelvban <2622415210@qq.com>
2020-11-24 14:49:35 +0800

    serialize finished

--- java/nowcoder/sword/SerializeBinaryTree.java
+++ java/nowcoder/sword/SerializeBinaryTree.java
@@ -4,22 +4,32 @@
 
  package nowcoder.sword;
 
- class BinaryTreeNode{
-     int val;
-     BinaryTreeNode left=null;
-     BinaryTreeNode right=null;
-
-     public BinaryTreeNode(int val){
-         this.val=val;
-     }
- }
 
  public class SerializeBinaryTree{
-
+   int index=-1;
     String Serialize(TreeNode root) {
+      if(root==null){
+        return "#";
+      }else{
+        return root.val+","+Serialize(root.left)+","+Serialize(root.right);
+      }
+      
         
     }
       TreeNode Deserialize(String str) {
+        String [] strings=str.split(",");
+        index++;
+        int len=strings.length;
+        if(index>len){
+          return null;
+        }
+        TreeNode treeNode=null;
+        if(!strings[index].equals("#")){
+          treeNode=new TreeNode(Integer.parseInt(strings[index]));
+          treeNode.left=Deserialize(str);
+          treeNode.right=Deserialize(str);
+        }
+        return treeNode;
          
     }
 
