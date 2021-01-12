417babb6689dd02d4f7c39b34f06e51aa928c6b8
kelvban <2622415210@qq.com>
2021-01-12 11:28:33 +0800

    dynamic planning solution

--- java/leetcode/LongestPalindromicSubstring_5.java
+++ java/leetcode/LongestPalindromicSubstring_5.java
@@ -1,7 +1,7 @@
 package leetcode;
 
 public class LongestPalindromicSubstring_5 {
-    public String longestPalindrome(String s) {
+    /*public String longestPalindrome(String s) {
         if(s.length()<2){
             return s;
         }
@@ -30,6 +30,46 @@ public class LongestPalindromicSubstring_5 {
             right--;
         }
         return true;
+    }*/
+
+    //dynamic planning
+    public String longestPalindrome(String s) {
+        // exception
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int begin = 0;
+
+        // dp[i][j] marks that s[i, j] is a palindromic string or not  
+        boolean[][] dp = new boolean[len][len];
+        char[] charArray = s.toCharArray();
+
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                // As long as dp[i][j] == true holds， s[i..j] is a palindromic string，the length of the palindromic and the starting and ending position are recorded
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
     }
 
     public static void main(String[] args) {
