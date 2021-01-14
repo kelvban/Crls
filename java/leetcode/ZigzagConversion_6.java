3c279138262f1866018f5efda4083a85da8ff390
kelvban <2622415210@qq.com>
2021-01-14 10:22:02 +0800

    zigzag

---
+++ java/leetcode/ZigzagConversion_6.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class ZigzagConversion_6 {
+
+    public String convert(String s, int numRows) {
+        char [] chars=s.toCharArray();
+        char [][] zigzag=new char[numRows][];   //up(col）=len/(3n-2)
+
+        return s;
+    }
+
+}
