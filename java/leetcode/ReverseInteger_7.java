9c9f938caad949fa135d459337e3263ac8e36b92
kelvban <2622415210@qq.com>
2021-04-06 11:26:06 +0800

    reverse integer

---
+++ java/leetcode/ReverseInteger_7.java
@@ -0,0 +1,14 @@
+
+public class ReverseInteger_7 {
+
+    public int reverse(int x) {
+        int result=0;
+        String xStr=String.valueOf(x);
+        char[] xChars=xStr.toCharArray();
+        if(xChars[0].equals([0-9])){
+
+        }
+        return result;
+    }
+
+}
