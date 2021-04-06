c07af81ee3839e1e54d4fbd4f3f107df9193ee5c
kelvban <2622415210@qq.com>
2021-04-06 17:37:27 +0800

    reverse finished

--- java/leetcode/ReverseInteger_7.java
+++ java/leetcode/ReverseInteger_7.java
@@ -1,14 +1,39 @@
+import java.util.Stack;
+import java.util.regex.Pattern;
 
 public class ReverseInteger_7 {
 
     public int reverse(int x) {
+        String pattern="[0-9]";
         int result=0;
         String xStr=String.valueOf(x);
         char[] xChars=xStr.toCharArray();
-        if(xChars[0].equals([0-9])){
-
+        Stack<Character> stack =new Stack<>();
+        if(Pattern.matches(pattern, String.valueOf(xChars[0]))){
+            for(Character c:xChars){
+                stack.push(c);
+            }
+        }else{
+            for(int i=1;i<xChars.length;i++){
+                stack.push(xChars[i]);
+            }
+        }
+        StringBuffer sb=new StringBuffer();
+        while(!stack.empty()){
+            sb.append(stack.pop());
+        }
+        if(Pattern.matches(pattern, String.valueOf(xChars[0]))){
+            result=Integer.parseInt(sb.toString());
+        }else{
+            result=Integer.parseInt(String.valueOf(xChars[0])+sb.toString());
         }
         return result;
+    }
+    public static void main(String[] args) {
+        int x=-108;
+        ReverseInteger_7 reverseInteger_7=new ReverseInteger_7();
+        int result=reverseInteger_7.reverse(x);
+        System.out.println(result);
     }
 
 }
